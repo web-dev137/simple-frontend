51e04313821dfa91dc647fca31fcc3e185cc2167
web-dev137 <daniyar.tuletsov.1994@mail.ru>
2025-09-26 16:20:31 +0700

    header

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.search').hover(
+    function() {$(this).children('img').attr("src","img/search-black.svg");}, 
+    function() {$(this).children('img').attr("src","img/search-yellow.svg");}
+);
+
+$('.bucket-btn').hover(
+    function() {$(this).children('img').attr("src","img/bucket-black.svg");}, 
+    function() {$(this).children('img').attr("src","img/bucket-yellow.svg");}
+)
