2062f30732ee2a68f3b217bb9215afe577fd0d28
web-dev137 <daniyar.tuletsov.1994@mail.ru>
2025-09-28 17:29:02 +0700

    landing section

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ $('.bucket-btn').hover(
     function() {$(this).children('img').attr("src","img/bucket-black.svg");}, 
     function() {$(this).children('img').attr("src","img/bucket-yellow.svg");}
 )
+
+$('.landing-page').hover(
+    function() {$(this).css("background-color","rgba(0, 0, 0, 20%)")},
+    function() {$(this).css("background-color","rgba(0, 0, 0, 4%)")}
+)
+    
