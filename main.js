4208b26b6cc1af900cec3700bf7fd6562e71990e
web-dev137 <daniyar.tuletsov.1994@mail.ru>
2025-09-30 18:56:45 +0700

    landing and footer section

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
-$('.search').hover(
-    function() {$(this).children('img').attr("src","img/search-black.svg");}, 
-    function() {$(this).children('img').attr("src","img/search-yellow.svg");}
+function changeIcon(parentElement, pathImgFind, pathImgReplace) {
+    $(parentElement).hover(
+        function(){$(this).children('img').attr("src", pathImgReplace)},
+        function(){$(this).children('img').attr("src", pathImgFind)}
+    );
+}
+
+function changeBackgroundColor(element, colorFind, colorReplace) {
+    $(element).hover(
+        function(){$(this).css("background-color", colorReplace)},
+        function(){$(this).css("background-color", colorFind)}
+    );
+}
+
+changeIcon(
+    '.footer-icon-1', 
+    'img/icons-footer/icon-footer-1-black.svg', 
+    'img/icons-footer/icon-footer-1-white.svg'
 );
 
-$('.bucket-btn').hover(
-    function() {$(this).children('img').attr("src","img/bucket-black.svg");}, 
-    function() {$(this).children('img').attr("src","img/bucket-yellow.svg");}
-)
+changeIcon(
+    '.footer-icon-2', 
+    'img/icons-footer/icon-footer-2-black.svg', 
+    'img/icons-footer/icon-footer-2-white.svg'
+);
 
-$('.landing-page').hover(
-    function() {$(this).css("background-color","rgba(0, 0, 0, 20%)")},
-    function() {$(this).css("background-color","rgba(0, 0, 0, 4%)")}
-)
-    
+changeIcon(
+    '.footer-icon-3', 
+    'img/icons-footer/icon-footer-3-black.svg', 
+    'img/icons-footer/icon-footer-3-white.svg'
+);
+
+changeIcon(
+    '.search', 
+    'img/search-yellow.svg', 
+    'img/search-black.svg'
+);
+
+changeIcon(
+    '.bucket-btn', 
+    'img/bucket-yellow.svg', 
+    'img/bucket-black.svg'
+);
+
+changeBackgroundColor(
+    '.landing-page', 
+    'rgba(0, 0, 0, 20%)', 
+    'rgba(0, 0, 0, 4%)'
+);
+
+$('#logo').hover(
+    function(){$(this).addClass("zoom")},
+    function(){$(this).removeClass("zoom")}
+);
